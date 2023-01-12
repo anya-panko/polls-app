34203ab2df5dbfa21165e0f35cf6abbe252b8da3
Anya <anya.panko00@gmail.com>
2023-01-12 21:55:24 +0300

    change settings.py

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -118,3 +118,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+TEST_EMAIL_USER = "anna.panko@test.com"
